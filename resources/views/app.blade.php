9f997cec6bf2882ffc5cc87dc25dbd2a6059a779
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2022-04-18 10:03:01 +0530

    install tailwind css

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <h1 class="flex h-screen w-full items-center justify-center">
+    Hello From <a href="https://github.com/jyotirmaybarman/laravel-tailwind-starter" target="_blank" class="underline text-blue-500 ml-2"> laravel -tailwind-starter</a>
+  </h1>
+</body>
+</html>
